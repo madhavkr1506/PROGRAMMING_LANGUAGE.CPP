16be6adc22db2a10ee26fbddcac6d6217e477e0c
Madhav Kumar <117391373+madhavkr1506@users.noreply.github.com>
2023-01-13 02:17:03 +0530

    Add files via upload

---
+++ data-types.cpp
@@ -0,0 +1,19 @@
+// data types :
+// primitive : integer, float, character, boolean.
+// derived : function, array, pointer, reference.
+// user-defined : class, structure, union, enum.
+// integer : 
+// size = 4 byte
+// range (unsigned) = 0 to 2^32 - 1
+// range (signed) = -2^31 to 2^31 - 1
+// float : 
+// size = 4 byte
+// eg : 4.322, 5.16 (upto 7 decimal digits)
+// double : size = 8 
+// upto 15 decimal digit.
+// char : 
+// size = 1 byte
+// bool : 
+// stores boolean value -> true(1) and false(0).
+// size = 1 byte
+
---
+++ data_types1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"size of int = "<<sizeof(int)<<endl;
+    cout<<"size of float = "<<sizeof(float)<<endl;
+    cout<<"size of char = "<<sizeof(char)<<endl;
+    cout<<"size of boolean = "<<sizeof(bool)<<endl;
+    cout<<"size of short int = "<<sizeof(short int)<<endl;
+    cout<<"size of long int = "<<sizeof(long int)<<endl;
+    cout<<"size of long long int = "<<sizeof(long long int)<<endl;
+return 0;
+}
